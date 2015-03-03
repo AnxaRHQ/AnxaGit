77d3c8a795485eb2ccfefd8efe53950bf0641466
Karlo de Grano <karlo@aujourdhui.com>
2015-03-03 14:47:52 +0800

    Initial Commit

---
+++ AnxaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnxaGit
+//
+//  Created by Karlo de Grano on 3/3/15.
+//  Copyright (c) 2015 Anxa Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
